67936ef39ded7481990a72bf2ef439abb9a52ca1
Solomia <mia.bobyak@gmail.com>
2024-08-09 22:58:39 +0300

    add repository and mapper

---
+++ src/main/java/car/sharing/repository/PaymentRepository.java
@@ -0,0 +1,16 @@
+package car.sharing.repository;
+
+import car.sharing.model.payment.Payment;
+import car.sharing.model.payment.Status;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findAllByRentalUserId(Long userId);
+    Optional<Payment> getBySessionId(String sessionId);
+    boolean existsByRentalUserIdAndStatus(Long userId, Status status);
+    boolean existsByRentalIdAndStatus(Long rentalId, Status status);
+}
