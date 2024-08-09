e003030bbcd32352a84f02a5b179e2b7d79b0bfd
Solomia <mia.bobyak@gmail.com>
2024-08-09 22:48:35 +0300

    add method

--- src/main/java/car/sharing/repository/RentalRepository.java
+++ src/main/java/car/sharing/repository/RentalRepository.java
@@ -3,9 +3,12 @@ package car.sharing.repository;
 import car.sharing.model.rental.Rental;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -17,4 +20,7 @@ public interface RentalRepository extends JpaRepository<Rental, Long> {
     List<Rental> getAllByUserIdAndActualReturnDateIsNotNull(Long userId, Pageable pageable);
 
     List<Rental> getAllByReturnDateBeforeAndActualReturnDateIsNull(LocalDate date);
+
+    @Query("SELECT r FROM Rental r JOIN FETCH r.car WHERE r.id = :rentalId")
+    Optional<Rental> findByIdWithCar(@Param("rentalId") Long rentalId);
 }
