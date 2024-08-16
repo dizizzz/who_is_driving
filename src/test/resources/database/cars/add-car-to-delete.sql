c706a85b2e08209fba1b49f51bf4260765437d06
Solomia <mia.bobyak@gmail.com>
2024-08-16 18:32:41 +0300

    add sql queries

---
+++ src/test/resources/database/cars/add-car-to-delete.sql
@@ -0,0 +1,2 @@
+INSERT INTO cars (id, model, brand, car_type, inventory, daily_fee, is_deleted)
+VALUES (3, 'Model 3', 'Tesla', 'SEDAN', 10, 25.00, FALSE);
---
+++ src/test/resources/database/users/add-users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (id, email, first_name, last_name, password, is_deleted)
+VALUES (2, 'sam@email.com', 'Sam', 'Smith', '$2a$10$7QnT56QwP7eEbKc8Of8b0OihmZG1v4nH0qucOa9YPb0Oeb.eKrVYy', FALSE);
+
+INSERT INTO users (id, email, first_name, last_name, password, is_deleted)
+VALUES (3, 'simon@email.com', 'Simon', 'Smith', '$2a$10$7QnT56QwP7eEbKc8Of8b0OihmZG1v4nH0qucOa9YPb0Oeb.eKrVYy', FALSE);
+
